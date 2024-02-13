e701d269add0a2f9f64aa5d2c20fc613897402c6
Muhammad Bin Aamir <81583119+wadader@users.noreply.github.com>
2024-02-13 18:11:37 +0500

    fix: fix auth router
    add cookie parser

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,41 @@
-console.log("hello word");
+import express from "express";
+import { Request, Response } from "express";
+import cors from "cors";
+import { env_Vars } from "./config/init";
+import { authRouter } from "./middleware/authentication/thirdwebAuth";
+import { CorsOptions } from "cors";
+
+const app = express();
+
+app.get("/", (_req: Request, res: Response) => {
+  res.json({
+    message: "Hello world",
+  });
+});
+
+app.use(express.json());
+
+export const corsOptions: CorsOptions = {
+  origin: [
+    `https://${env_Vars.Auth.THIRDWEB_AUTH_DOMAIN}`,
+    `http://${env_Vars.Auth.THIRDWEB_AUTH_DOMAIN}`,
+    "http://localhost:5174",
+  ],
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+
+app.use("/api/v1/auth", authRouter);
+
+app.get("*", function (_req: Request, res: Response) {
+  console.log("404ing");
+  res.status(404).json({ status: "404" });
+});
+
+// railway provides this PORT directly
+const PORT = process.env.PORT || env_Vars.app.BACKEND_PORT;
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT} - rpslz`);
+});
