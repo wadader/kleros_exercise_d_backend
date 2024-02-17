31bb41738b3b4589a80c6e292d37f4b69c2a88a7
Muhammad Bin Aamir <81583119+wadader@users.noreply.github.com>
2024-02-17 11:31:14 +0500

    feat: add .env for handling test chain

--- src/config/ethClients.ts
+++ src/config/ethClients.ts
@@ -1,5 +1,20 @@
-import { createPublicClient, http } from "viem";
+import { createPublicClient, defineChain, http } from "viem";
 import { sepolia } from "viem/chains";
+
+const ganache = defineChain({
+  id: 1337,
+  name: "Ganache",
+  nativeCurrency: {
+    decimals: 18,
+    name: "Ganache Ether",
+    symbol: "GETH",
+  },
+  rpcUrls: {
+    default: {
+      http: ["http://127.0.0.1:7545"],
+    },
+  },
+});
 
 const CHAINS = {
   sepolia: {
@@ -10,8 +25,15 @@ const CHAINS = {
 
 const SELECTED_CHAIN = CHAINS.sepolia;
 
-export const getPublicClient = (infuraEndpoint: string) =>
-  createPublicClient({
+export const getPublicClient = (infuraEndpoint: string) => {
+  if (process.env.NODE_ENV == "dev")
+    return createPublicClient({
+      chain: ganache,
+      transport: http(),
+    });
+
+  return createPublicClient({
     chain: SELECTED_CHAIN.viemChain,
     transport: SELECTED_CHAIN.getHttpSocketUrl(infuraEndpoint),
   });
+};
