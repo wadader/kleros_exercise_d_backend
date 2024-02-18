ed30189bce3d0047eb167c161d6f132c6c5caae9
Muhammad Bin Aamir <81583119+wadader@users.noreply.github.com>
2024-02-19 01:37:46 +0500

    feat: add generated migrations

---
+++ src/database/drizzle/schema/generated/0000_bitter_fabian_cortez.sql
@@ -0,0 +1,26 @@
+CREATE TABLE IF NOT EXISTS "logins" (
+	"logins" text PRIMARY KEY NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "rpslz_games" (
+	"tx_hash" char(66) PRIMARY KEY NOT NULL,
+	"created_contract_address" char(42) NOT NULL,
+	"salt_id" integer NOT NULL,
+	"joiner_address" char(42) NOT NULL,
+	"is_game_over" boolean DEFAULT false NOT NULL,
+	CONSTRAINT "rpslz_games_created_contract_address_unique" UNIQUE("created_contract_address"),
+	CONSTRAINT "rpslz_games_salt_id_unique" UNIQUE("salt_id")
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "salts" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"salt" numeric(78) NOT NULL,
+	"requested_by_address" char(42) NOT NULL,
+	CONSTRAINT "salts_salt_unique" UNIQUE("salt")
+);
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "rpslz_games" ADD CONSTRAINT "rpslz_games_salt_id_salts_id_fk" FOREIGN KEY ("salt_id") REFERENCES "salts"("id") ON DELETE no action ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
