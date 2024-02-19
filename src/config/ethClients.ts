45996aa2ad66aac7e2ef08380e12cad9f5280a4f
Muhammad Bin Aamir <81583119+wadader@users.noreply.github.com>
2024-02-19 12:32:28 +0500

    chore: shift to official contract

--- src/config/ethClients.ts
+++ src/config/ethClients.ts
@@ -1,20 +1,20 @@
-import { createPublicClient, defineChain, http } from "viem";
+import { createPublicClient, http } from "viem";
 import { sepolia } from "viem/chains";
 
-const ganache = defineChain({
-  id: 1337,
-  name: "Ganache",
-  nativeCurrency: {
-    decimals: 18,
-    name: "Ganache Ether",
-    symbol: "GETH",
-  },
-  rpcUrls: {
-    default: {
-      http: ["http://127.0.0.1:7545"],
-    },
-  },
-});
+// const ganache = defineChain({
+//   id: 1337,
+//   name: "Ganache",
+//   nativeCurrency: {
+//     decimals: 18,
+//     name: "Ganache Ether",
+//     symbol: "GETH",
+//   },
+//   rpcUrls: {
+//     default: {
+//       http: ["http://127.0.0.1:7545"],
+//     },
+//   },
+// });
 
 const CHAINS = {
   sepolia: {
@@ -26,12 +26,6 @@ const CHAINS = {
 const SELECTED_CHAIN = CHAINS.sepolia;
 
 export const getPublicClient = (infuraEndpoint: string) => {
-  if (process.env.NODE_ENV == "dev")
-    return createPublicClient({
-      chain: ganache,
-      transport: http(),
-    });
-
   return createPublicClient({
     chain: SELECTED_CHAIN.viemChain,
     transport: SELECTED_CHAIN.getHttpSocketUrl(infuraEndpoint),
--- src/controllers/games/games.ts
+++ src/controllers/games/games.ts
@@ -388,7 +388,7 @@ async function getIsFetchedByteCodeCorrect(
   const fetchedBytecode = await games.publicClient.getBytecode({
     address: contractAddr,
   });
-  if (fetchedBytecode === RPS_ARTIFACT.fifteenSecondDeployedVytecode)
+  if (fetchedBytecode === RPS_ARTIFACT.deployedBytecode)
     return true;
   return false;
 }
